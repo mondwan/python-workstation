21d20ae11ea3f0febf4f81a2f79d9231b0ed2703
Mond Wan <victor2wen@gmail.com>
2013-12-22 12:18:52 +0800

    Upload first exercises

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print "Hello World!";
+print "Hello Again";
+print "I hate typing this";
+print "This is not fun.";
+print "how come it is not the end of this exercise";
+print "wahahahahahahah";
+print "oops let's end this boring exercise";
---
+++ ex2.py
@@ -0,0 +1,5 @@
+#Comment1
+print "code 1";
+#comment 2
+print "code 2";
+
